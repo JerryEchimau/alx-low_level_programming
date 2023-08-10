a620f19d574023efdc285067c5e6a3b557d25f77
Jerry John <jerryjohne77@gmail.com>
2023-08-10 16:46:03 -0700

    edited 4-puts.c to add description for main and return functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - a program that prints the words shown using puts
+ * program to end with a 0
+ *
+ * return - 0
+ */
 int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
