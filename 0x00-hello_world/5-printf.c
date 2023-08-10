b99a342f3ee60307d17d80ce9d87e6b6c05dbd6d
Jerry John <jerryjohne77@gmail.com>
2023-08-10 16:54:58 -0700

    edited the c files while adding betty style to explain the code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * main - printing using printf (by adding \n at the end)
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
