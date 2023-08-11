b0f874cc302c0d0a0006564a1d47a0381e426c1d
Jerry John <jerryjohne77@gmail.com>
2023-08-10 17:01:11 -0700

    edited the c files while adding betty style to explain the code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - printing using printf (by adding \n at the end)
+ * main: printing strings using printf (by adding \n at the end)
  * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - using the sizeoff command in c
+ * main: using the sizeoff command in c
  * Return: 0
  */
 int main(void)
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
