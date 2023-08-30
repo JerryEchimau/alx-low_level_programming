73c4790797e6e3d8a19963847db5b1239acc1b0b
Jerry John <jerryjohne77@gmail.com>
2023-08-30 07:35:55 -0700

    finished rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
- * *rot13 - encodes a string using rot 13
- * @s: source string
- * Return: string 's' rotated
+ * rot13 - rotate characters 13 places in the alphabet
+ * @s: string
+ * Return: string `s` rotated
  */
+
 char *rot13(char *s)
 {
 	int i;
-
-	char rot13_lower[] = "nopqrstuvwxyzabcdefghijklm";
-	char rot13_upper[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char upper[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char lower[] = "nopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
 			s[i] = (s[i] - 65 > 25) ?
-				rot13_lower[s[i] - 97] : rot13_upper[s[i] - 65];
+				lower[s[i] - 97] : upper[s[i] - 65];
 		}
 	}
 	return (s);
