2115057a0967b1e6e673b1f0134a0a69c73dc3a1
Jerry John <jerryjohne77@gmail.com>
2023-08-31 19:21:10 -0700

    finished extra assignments

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,50 +1,55 @@
 #include "main.h"
 
+int check_palindrome(char *s, int start, int end);
+int _strlen(char *s);
+
 /**
- * is_palindrome - check if a string is a palindrome
- * @s: char array string
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to be checked
  * Return: 1 if palindrome, 0 if not
  */
-
 int is_palindrome(char *s)
 {
-	int length;
+	int length = _strlen(s);
 
-	length = get_length(s) - 1;
-
-	return (my_pal(s, --length));
+	return (check_palindrome(s, 0, length - 1));
 }
 
 /**
- * get_length - gets length of string
+ * _strlen - checks the length of a string
  * @s: string
- * Return: return length of string
+ * Return: length of string
  */
-
-int get_length(char *s)
+int _strlen(char *s)
 {
-	if (*s == '\0')
-		return (1);
-	else
-		return (1 + get_length(++s));
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
 
 /**
- * my_pal - recursive check of palindrome
- * @s: string
- * @l: length of string
+ * check_palindrome - performs palindrom checks
+ * @s: string ro be checked
+ * @start: string index for checking
+ * @end: ending f index for checking
  * Return: 1 if palindrome, 0 if not
  */
-
-int my_pal(char *s, int l)
+int check_palindrome(char *s, int start, int end)
 {
-	if (*s == *(s + l))
+	if (start >= end)
 	{
-		if (l <= 0)
-			return (1);
-		else
-			return (my_pal(++s, l - 2));
+		return (1);
+	}
+	else if (s[start] != s[end])
+	{
+		return (0);
 	}
 	else
-		return (0);
+	{
+		return (check_palindrome(s, start + 1, end - 1));
+	}
 }
