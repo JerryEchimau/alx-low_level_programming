7d4ae497f11ae536ad5818de2b3d7a34e8cd6700
Jerry John <jerryjohne77@gmail.com>
2023-09-08 16:52:16 -0700

    edited 101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -57,14 +57,14 @@ void printNumbers(long num)
 }
 
 /**
- * main - takes in command line arguments
+ * validateInput - validates the error messages
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 if successful and 98 if error
  */
-int main(int argc, char *argv[])
+int validateInput(int argc, char *argv[])
 {
-	long result, num1, num2;
+	long num1, num2;
 
 	/* checks that there are only two inputs */
 	if (argc != 3)
@@ -77,7 +77,6 @@ int main(int argc, char *argv[])
 		_putchar('\n');
 		return (98);
 	}
-
 	/* checks if num1 and num2 are digits */
 	if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
 	{
@@ -89,11 +88,9 @@ int main(int argc, char *argv[])
 		_putchar('\n');
 		return (98);
 	}
-
 	/* convert command line arguments to long integers */
 	num1 = atol(argv[1]);
 	num2 = atol(argv[2]);
-
 	/* check if num1 and num2 are positive numbers */
 	if (num1 <= 0 || num2 <= 0)
 	{
@@ -105,6 +102,26 @@ int main(int argc, char *argv[])
 		_putchar('\n');
 		return (98);
 	}
+	return (0);
+}
+
+/**
+ * main - takes in command line arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful and 98 if error
+ */
+int main(int argc, char *argv[])
+{
+	long result;
+	long num1;
+	long num2;
+
+	if (validateInput(argc, argv) != 0)
+		return (98);
+
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
 
 	/* final calculation and printing the results */
 	result = multiply(num1, num2);
