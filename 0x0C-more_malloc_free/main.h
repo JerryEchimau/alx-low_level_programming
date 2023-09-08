bbc4358dafa1f518651617765ee04d60c65fa119
Jerry John <jerryjohne77@gmail.com>
2023-09-08 16:56:09 -0700

    edited mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,10 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int isNumeric(const char *str);
+long multiply(long num1, long num2);
+void printNumbers(long num);
+int validateInput(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
