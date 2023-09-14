d373ef5ec33fb59e38e0eb6a42a4cd03b7310071
Jerry John <jerryjohne77@gmail.com>
2023-09-13 19:45:27 -0700

    edited the header to include appropriate libraries

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #ifndef CALC_H
 #define CALC_H
 
@@ -13,6 +15,7 @@ typedef struct op
 } op_t;
 
 /* Function prototypes */
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
