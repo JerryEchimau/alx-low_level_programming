b0418554b0f67766f805fc5d97d7110f25889005
Jerry John <jerryjohne77@gmail.com>
2023-09-21 20:02:13 -0700

    advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-/**
- * pre_main - Function to print the message before main.
- */
-void pre_main(void) __attribute__((constructor));
-
-void pre_main(void)
-{
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
+void __attribute__((constructor)) hare(void);
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-int main(void)
+void hare(void)
 {
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
