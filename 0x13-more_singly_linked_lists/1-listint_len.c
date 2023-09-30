92228a69e1934fe0eada8e93d448a3d1acf79f04
Jerry John <jerryjohne77@gmail.com>
2023-09-30 16:05:27 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * listint_len - returns the no o flements in a linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: no of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
