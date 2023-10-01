fce973413c636702ad5c7847e471bc4978e39008
Jerry John <jerryjohne77@gmail.com>
2023-09-30 18:39:27 -0700

    new edit for task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	node_count = 0;
 
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		node_count++;
 	}
 
-	return (nodes);
+	return (node_count);
 }
