f73734cd8309d0613ec6404a51ec2e786a839ee2
Jerry John <jerryjohne77@gmail.com>
2023-10-03 03:53:30 -0700

    task 12 edited

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,34 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: A pointer to the head of the list.
- *
- * Return: The number of nodes in the list.
+ * print_listint_safe - Print a `listint_t` linked list including mem addresses
+ * @head: head of linked list
+ * Description: Go through the list only once.
+ * Return: number of nodes in list. If fails, exit with status 98.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise = head, *hare = head;
-	size_t count = 0;
+	const listint_t *current;
+	size_t count;
+	const listint_t *hold;
 
-	while (hare != NULL && hare->next != NULL)
+	current = head;
+	if (current == NULL)
+		exit(98);
+
+	count = 0;
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-		hare = hare->next->next;
+		hold = current;
+		current = current->next;
+		count++;
+		printf("[%p] %d\n", (void *)hold, hold->n);
 
-		if (tortoise == hare)
+		if (hold < current)
 		{
-			/* Loop detected, exit with status 98 */
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			exit(98);
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
 		}
-		count++;
-	}
-
-	/* Print the last node (if it exists) and return the node count */
-	if (tortoise != NULL)
-	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		count++;
 	}
 
 	return (count);
