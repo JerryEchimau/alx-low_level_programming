cb4a6a30621b1a81947127d45616e96a5a669721
Jerry <jerryjohne77@gmail.com>
2023-11-01 11:22:49 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,91 +1,48 @@
 #include "main.h"
 
 /**
- * print_error - prints error
- * @message: error message
- * @arg: the argument to be includd in the message
- */
-void print_error(const char *message, const char *arg)
-{
-	dprintf(2, message, arg);
-	exit(97);
-}
-
-/**
- * open_source_file - open the source of the file for reading
- * @filename: name of the source file
- *
- * Return: the file descriptor of the source file
- */
-int open_source_file(const char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		print_error("Error: Can't read from the filexx %s\n", filename);
-	return (fd);
-}
-
-/**
- * open_dest_file - open destination file for writing
- * @filename: name of the destination file
- *
- * Return: file descriptor of the det file
- */
-int open_dest_file(const char *filename)
-{
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (fd == -1)
-		print_error("Error: Can't write to %s\n", filename);
-	return (fd);
-}
-
-/**
- * copy_file - copy content from source to dest
- * @fd_from: file descriptor of source file
- * @fd_to: file descriptor of dest file
- */
-void copy_file(int fd_from, int fd_to)
-{
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			print_error("Error: Can't write to destination file\n", "");
-	}
-
-	if (bytes_read == -1)
-		print_error("Error: Can't read from the source file\n", "");
-}
-
-/**
- * main - entry point for the cp program
+ * main - create the copy bash script
  * @argc: argument count
- * @argv: array of command line arguments
- *
- * Return: 0 on success, exit with 1on failure
+ * @argv: arguments as strings
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	int input_fd, output_fd, istatus, ostatus;
+	char buf[MAXSIZE];
+	mode_t mode;
 
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (argc != 3)
-		print_error("Usage: %s file_from file_to\n", argv[0]);
-
-	fd_from = open_source_file(argv[1]);
-	fd_to = open_dest_file(argv[2]);
-
-	copy_file(fd_from, fd_to);
-
-	if (close(fd_from) == -1)
-		print_error("Error: Can't close fd %s\n", strerror(errno));
-	if (close(fd_to) == -1)
-		print_error("Error: Can't close fd %s\n", strerror(errno));
-
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+	input_fd = open(argv[1], O_RDONLY);
+	if (input_fd == -1)
+		dprintf(SE, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (output_fd == -1)
+		dprintf(SE, "Error: Can't write to %s\n", argv[2]), exit(99);
+	do {
+		istatus = read(input_fd, buf, MAXSIZE);
+		if (istatus == -1)
+		{
+			dprintf(SE, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (istatus > 0)
+		{
+			ostatus = write(output_fd, buf, (ssize_t) istatus);
+			if (ostatus == -1)
+			{
+				dprintf(SE, "Error: Can't write to %s\n", argv[2]);
+				exit(99);
+			}
+		}
+	} while (istatus > 0);
+	istatus = close(input_fd);
+	if (istatus == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
+	ostatus = close(output_fd);
+	if (ostatus == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFER_SIZE 1024
+#define MAXSIZE 1204
+#define SE STDERR_FILENO
 
 /* libraries */
 #include <stdio.h>
@@ -16,10 +17,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(const char *message, const char *arg);
-int open_source_file(const char *filename);
-int open_dest_file(const char *filename);
-void copy_file(int fd_from, int fd_to);
 int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
