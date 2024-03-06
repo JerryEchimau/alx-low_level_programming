84a5675a0c6f1ba6f025bbbbf7e8aeb7d5040a9d
Jerry <jerryjohne77@gmail.com>
2024-03-06 05:52:03 -0800

    Handled Betty

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,39 @@
-#include "main.h"
+#include "main"
 
-/*
- * print_number - prints an integer
- * @n : number to be printed
-*/
+/**
+ * print_number - prints an integer using _putchar
+ * @n: number to be printed
+ *
+ */
+
 void print_number(int n)
 {
-  int divisor = 1;
+	int divisor = 1;
 
-  /* handle zero */
-  if (n == 0)
-    {
-      _putchar('0');
-      return;
-    }
+	/* handle zero */
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-  /* negative numbers */
-  if (n < 0)
-    {
-      _putchar('-');
-      n = (-n);
-    }
+	/* negative numbers */
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (-n);
+	}
 
-  /* get the number of digits using divisor */
-  while (n / divisor >= 10)
-    {
-      divisor *= 10;
-    }
+	/* get the number of digits using divisor */
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
 
-  /* print each digit */
-  while (divisor != 0)
-    {
-      _putchar((n / divisor) % 10 + '0');
-      divisor /= 10;
-    }
+	/* print each digit */
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) % 10 + '0');
+		divisor /= 10;
+	}
 }
