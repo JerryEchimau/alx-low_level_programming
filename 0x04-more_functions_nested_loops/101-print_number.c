88dc116b08698a77f0a3df8a44de4d6e524e9fd4
Jerry <jerryjohne77@gmail.com>
2024-03-06 05:40:59 -0800

    did last advanced task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/*
+ * print_number - prints an integer
+ * @n : number to be printed
+*/
+void print_number(int n)
+{
+  int divisor = 1;
+
+  /* handle zero */
+  if (n == 0)
+    {
+      _putchar('0');
+      return;
+    }
+
+  /* negative numbers */
+  if (n < 0)
+    {
+      _putchar('-');
+      n = (-n);
+    }
+
+  /* get the number of digits using divisor */
+  while (n / divisor >= 10)
+    {
+      divisor *= 10;
+    }
+
+  /* print each digit */
+  while (divisor != 0)
+    {
+      _putchar((n / divisor) % 10 + '0');
+      divisor /= 10;
+    }
+}
