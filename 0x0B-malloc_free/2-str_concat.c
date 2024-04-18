1b092df23ecef29e92208e7df58d91fdc648f364
Jerry John <139127221+JerryEchimau@users.noreply.github.com>
2024-04-18 14:14:53 +0300

    Updated README

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * *str_concat - concatenates two strings
+ * str_concat - concatenates two strings
  * @s1: first string
- * @s2: second string to be concatenated
- * Return: pointer to a new space with contents of s1 and s2
- * Null if insufficient memory
- */
+ * @s2: second string
+ * 
+ * Return: pointer to the concatenated strings, NULL upon failure
+*/
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2, i, j;
-	char *conc;
+    int i, len1 = 0, len2 = 0;
+    char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-	{
-		;
-	}
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-	{
-		;
-	}
+    for (i = 0; s1[i] != '\0'; i++)
+        len1++;
+    for (i = 0; s2[i] != '\0'; i++)
+        len2++;
+    
+    if (!(s1 || s2)) /* check if any of the pointers is NULL */
+        return (NULL);
+    
+    result = malloc((len1 + len2 + 1) * sizeof(char));/* allocate memory for the new string */
 
-	conc = malloc(((sizeof(*s1) * len1) + (sizeof(*s2) * len2)) + 1);
-	if (conc == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-	{
-		conc[i] = s1[i];
-	}
-	for (j = 0; j < len2; j++)
-	{
-		conc[i + j] = s2[j];
-	}
-	conc[i + j] = '\0';
-	return (conc);
+    if (!result)   /* check if allocation was successful */
+        return (NULL);
+    
+    /*concatenate*/
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
+    for (i = 0; i < len2; i++)
+        result[len1 + i] = s2[i];
+	
+    result[len1 + len2] = '\0';      /* add null terminator at end of new string */
+    
+    return (result);                 /* return pointer to the new string */
 }
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,32 @@
-README File for 0x0B-malloc_free Assignment
-Author: Jerry E. John
+# 0x0B. C - malloc, free
+
+- In this project, we learnt about dynamic memory allocation and allocating memory at runtime
+- Here are the tasks in this project:
+
+## Tasks
+
+### 0. Float like a butterfly, sting like a bee
+
+- First, allocate mmory to a pointer through malloc
+- Then iterate through each memory location and copy char c
+
+### 1. The woman who has no imagination has no wings
+
+- Here, we first get the length of str and the allocate memory to a duplicate variable using the length of str
+- We then copy the contents of str to dup
+
+### 2. He who is not courageous enough to take risks will accomplish nothing in life
+
+- First, find the length of each string then allocate memory using len1 + len2 + 1(for a NULL terminator)
+- Then concatenate the two strings and include the null terminator
+
+### 3. If you even dream of beating me you'd better wake up and apologize
+
+- Here, we first allocate memory to an integer pointer (with height size) and then iterate through it and allocate memory to an integer (with width size).
+- we then iterate through the second allocated memory and initiate every component with 0
+
+### 4. It's not bragging if you can back it up
+
+- Here, we free both the inner memory ``grid[i]` and the outer memory ``grid``
+
+Author: Jerry Echimau
